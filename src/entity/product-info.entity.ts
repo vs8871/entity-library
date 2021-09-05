02a7475c18bd6357eedd7f775b23a5473e9a38c8
vs8871 <40366182+vs8871@users.noreply.github.com>
2021-09-06 00:15:45 +0530

    updated by autobot

---
+++ src/entity/product-info.entity.ts
@@ -0,0 +1,21 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity({ name: 'product_info' })
+export class ProductInfo {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'integer', name: 'letter_id' })
+    letterId: number;
+
+
+    @Column({ type: 'date', name: 'receving_date' })
+    recevingDate: date;
+
+    @Column({ type: 'integer', name: 'items' })
+    items: number;
+
+    @Column({ type: 'text', name: 'comment' })
+    comment: string;
+}
