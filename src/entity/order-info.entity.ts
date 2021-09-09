2a52ef5331b6572f5640c5c20f44c52a2485a02b
vs8871 <40366182+vs8871@users.noreply.github.com>
2021-09-09 16:48:04 +0530

    updated by autobot

---
+++ src/entity/order-info.entity.ts
@@ -0,0 +1,32 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity({ name: 'order_info' })
+export class OrderInfo {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'integer', name: 'product_id' })
+    productId: number;
+
+    @Column({ type: 'date', name: 'delivery_date' })
+    deliveryDate: date;
+
+    @Column({ type: 'integer', name: 'quantity' })
+    quantity: number;
+
+    @Column({ type: 'text', name: 'feedback' })
+    feedback: string;
+
+    @Column({ type: 'varchar', name: 'address' })
+    address: string;
+
+    @Column({ type: 'varchar', name: 'order_by' })
+    orderBy: string;
+
+    @Column({ type: 'int', name: 'pin_code' })
+    pinCode: undefined;
+
+    @Column({ type: 'varchar', name: 'address' })
+    address: string;
+}
