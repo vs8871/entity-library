217bc8e6cc3d385cbb7a95115ba1a5140ae440b3
vs8871 <40366182+vs8871@users.noreply.github.com>
2021-09-09 16:57:06 +0530

    updated by autobot

--- src/entity/product-info.entity.ts
+++ src/entity/product-info.entity.ts
@@ -9,12 +9,6 @@ export class ProductInfo {
     @Column({ type: 'integer', name: 'letter_id' })
     letterId: number;
 
-    @Column({ type: 'date', name: 'receving_date' })
-    recevingDate: date;
-
-    @Column({ type: 'integer', name: 'items' })
-    items: number;
-
     @Column({ type: 'text', name: 'comment' })
     comment: string;
 }
